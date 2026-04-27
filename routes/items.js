3de489521466669ddcc188f3c8c64db311c1bd4e
JananiKavindya <kavindyajanani123@gmail.com>
2026-04-28 04:37:59 +0530

    last updated

--- routes/items.js
+++ routes/items.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Item = require('../models/Item');
+const Item = require('../model/item');
 // GET all items
 router.get('/', async (req, res) => {
 try {
